8ff7a5d88ee47eca3c513a49529b3682aba2e594
Marcelo Asfor <marceloasfor@hotmail.com>
2022-06-01 18:52:48 -0300

    inital ingestDB

---
+++ ingestDB.sql
@@ -0,0 +1,41 @@
+USE FeriasDB;
+
+INSERT INTO Sexo (descricao)
+VALUES
+('Feminino'),
+('Masculino'),
+('Outro');
+
+INSERT INTO NecessidadeEspecial (tipoNecessidade)
+VALUES
+('Cadeira de Rodas'),
+('Criança de colo'),
+('Deficiência Auditiva'),
+('Deficiência Visual'),
+('Gestante'),
+('Mobilidade Reduzida'),
+('Outro');
+
+INSERT INTO tipoPontoTuristico (tipo)
+VALUES
+('Bar'),
+('Cinema'),
+('Estádio/Evento Esportivo'),
+('Eventos'),
+('Restaurante'),
+('Monumentos'),
+('Parque'),
+('Passeios'),
+('Museu'),
+('Teatro/Palestra');
+
+INSERT INTO TipoDespesa (tipoDespesa)
+VALUES
+('Passagem'),
+('Hospedagem'),
+('Aluguel de Carro'),
+('Alimentação'),
+('Mobilidade'),
+('Lazer/Ingressos'),
+('Saúde/Sinistros');
+
